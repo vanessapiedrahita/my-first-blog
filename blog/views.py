bcfe8882fe597898bf196f9e8373cc0b6cf6fe77
vanessapiedrahita <vanesa.piedrahita@udea.edu.co>
2019-08-31 16:40:56 -0500

    Change the HTML for the site

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
+from .models import Post
 from django.shortcuts import render
+from django.utils import timezone
 
 def post_list(request):
+	Post.objects.all().order_by('-published_date')
 	return render(request, 'blog/post_list.html', {})
